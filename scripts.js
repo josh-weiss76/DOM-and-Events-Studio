538d526678ea171bc57f9172d9b925a4561711d6
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-05-20 10:59:29 -0400

    updated window load check

--- scripts.js
+++ scripts.js
@@ -9,6 +9,10 @@ let left = null;
 let right = null;
 let rocket = null;
 
+window.addEventListener("load", function(){
+    console.log("Window loaded");
+  });
+
 function init () {
     missionAbort = document.getElementById("missionAbort");
     button = document.getElementById("takeoff");
@@ -18,10 +22,8 @@ function init () {
     left = document.getElementById("left");
     right = document.getElementById("right");
     rocket = document.getElementById("rocket");
-
-    window.addEventListener("load", function(event){
-      console.log("Window loaded");
-    })
+    let x = document.getElementById("rocket").offsetTop;
+    let y = document.getElementById("rocket").offsetLeft;
 
     button.addEventListener("click", function(event){
       let response = window.confirm("Confirm that the shuttle is ready for takeoff.")
@@ -29,6 +31,7 @@ function init () {
         document.getElementById("flightStatus").innerHTML = "Shuttle in flight.";
         document.getElementById("shuttleBackground").style = "background: blue";
         document.getElementById("spaceShuttleHeight").innerHTML = Number(10000);
+        console.log("button clicked");
       };
     });
 
@@ -37,7 +40,7 @@ function init () {
       document.getElementById("flightStatus").innerHTML = "The shuttle has landed.";
       document.getElementById("shuttleBackground").style = "background: green";
       document.getElementById("spaceShuttleHeight").innerHTML = Number(0);
-    })
+    });
 
     missionAbort.addEventListener("click", function(event){
       let response = window.confirm("Confirm that you want to abort the mission.")
@@ -52,16 +55,13 @@ function init () {
         let height = Number(document.getElementById("spaceShuttleHeight").innerHTML);
         height = height + 10000;
         document.getElementById("spaceShuttleHeight").innerHTML = height;
-        let location = document.getElementById("rocket").offsetTop;
-        location = location - 10;
-        document.getElementById("rocket").style.top = location + "px";
-    })
+    });
 
     down.addEventListener("click", function(event){
         let height = Number(document.getElementById("spaceShuttleHeight").innerHTML);
         height = height - 10000;
         document.getElementById("spaceShuttleHeight").innerHTML = height;
-    })
+    });
 
 }
 
